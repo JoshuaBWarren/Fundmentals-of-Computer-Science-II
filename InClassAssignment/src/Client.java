32b8221efbca5b7dc82161a2dc1a66b2c5f9d0f8
Joshua Warren <WarrenJB86@live.com>
2018-04-11 15:13:40 -0700

    In class assignment
    Done and comments added

--- InClassAssignment/src/Client.java
+++ InClassAssignment/src/Client.java
@@ -1,3 +1,10 @@
+/*
+ * Joshua Warren
+ * CS 211
+ * 4/11/2018
+ * In class assignment
+ */
+
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
@@ -14,12 +21,15 @@ public class Client {
 			numbers.add(n);
 			}
 		input.close();
-		System.out.println(numbers);
-		//filterEvens(numbers);
-		//System.out.println(numbers);
 		
-		//findLargest(numbers);
+		// Tests!
+		System.out.println(numbers);
+		filterEvens(numbers);
+		
+		System.out.println(numbers);
 		System.out.println(findLargest(numbers));
+		System.out.println(findSmallest(numbers));
+		System.out.println(average(numbers));
 	}
 		// Removes all elements with even values from the given list.
 		public static void filterEvens(ArrayList<Integer> list) {
@@ -34,15 +44,19 @@ public class Client {
 		// finds the largest integer within the array list.
 		public static int findLargest(ArrayList<Integer> list) {
 
-			int answer = 0;
+			// grab the first value within the array list
+			int answer = list.get(0);
 			
+			// loop through the array list and compare it to the first value
+			// within.  If the current is greater than the answer, then set the
+			// answer value as the current value.
 			for(int i = 0; i <= list.size() - 1; i++) {
 				int current = list.get(i);
-				answer = current;
-				if(answer > current) {
+				if(answer < current) {
 					answer = current;
 				}
 			}
+			// return the largest integer
 			return answer;
 			
 		}
@@ -50,30 +64,42 @@ public class Client {
 		// finds the smallest integer within the array list.
 		public static int findSmallest(ArrayList<Integer> list) {
 			
-			int answer = 0;
+			// grab the first value within the array list
+			int answer = list.get(0);
 			
-			for(int i = 0; i <= list.size(); i++) {
+			// loop through the array list and compare it to the first value
+			// within.  If the current is less than the answer, then set the
+			// answer value as the current value.
+			for(int i = 0; i <= list.size() - 1; i++) {
 				int current = list.get(i);
-				if(answer < current) {
+				if(answer > current) {
 					answer = current;
 				}
 			}
+			
+			// return the smallest integer
 			return answer;
 		}
 		
 		// finds the average of the integers within the array list.
 		public static int average(ArrayList<Integer> list) {
 			
+			// set the values we'll need to do the average of integers 
+			// within the array list.
 			int average = 0;
 			int sum = 0;
 			int size = list.size();
 			
-			for(int i = 0; i <= list.size(); i++) {
+			// loop through the array list and add all the integers together.
+			for(int i = 0; i <= list.size() - 1; i++) {
 				int n = list.get(i);
 				sum += n;
 			}
+			
+			// perform the average calculation.
 			average = sum / size;
 			
+			// return the result of the average calculation.
 			return average;
 		}
 }
