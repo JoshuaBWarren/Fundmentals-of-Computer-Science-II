d1a9dc01948a628cd00019949f9c1f7924d3cf52
WarrenJB86 <WarrenJB86@live.com>
2018-05-11 14:21:11 -0700

    practice
    meh

--- RandomTesting/src/Random.java
+++ RandomTesting/src/Random.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Random {
 	
@@ -6,13 +7,43 @@ public class Random {
 		System.out.println(SimpleAdding(4));
 		System.out.println(LetterCapitalize("i am the batman"));
 		
+		String[] test = new String[] {"zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"};
+		
+		//System.out.println(test.toString());
+		System.out.println(longestConsec(test, 2));
+		
 	}
 
+	public static int squareDigits(int n) {
+		
+		
+		
+		String number = Integer.toString(n);
+		//String[] storage = new String[];
+		number.split("");
+		
+		for(int i = 0; i < number.length(); i++) {
+			int temp = Integer.parseInt(number[i]);
+		}
+	  }
 	
-	
-	
-	
-	
+	public static String longestConsec(String[] strarr, int k) {
+        
+		String answer = "";
+
+        for(int i = 0; i < k; i++) {
+        	String tempString = "";
+        	for(int j = 0; j < strarr.length; j++) {
+        		int temp = 0;
+        		if(strarr[j].length() > temp) {
+        			tempString = strarr[j];
+        		}
+        	}
+			answer = answer + " " + tempString;
+    }
+        return answer;
+}
+
 	
 	
 	public static int SimpleAdding(int num) { 
